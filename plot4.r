4c8a9f0ba6ef72efff45838723925f1c4fae1e27
Natalia Boldyrev <natalia@wks-15-11.mpi-inf.mpg.de>
2016-09-19 10:52:45 +0200

    added R code and png files

---
+++ plot4.r
@@ -0,0 +1,51 @@
+#column types
+colClss <- c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+
+
+#reading the data, interpreting '?' as Na's
+consumption <- read.csv("data/household_power_consumption.txt", 
+                        colClasses = colClss, sep = ';', na.strings=c("?"),
+                        nrows=2075259)
+
+						
+#concatenating date and time for plots 2-4				
+consumption$full_date = paste(consumption$Date, consumption$Time, sep=" ")
+consumption$full_date = strptime(consumption$full_date, "%d/%m/%Y %H:%M:%S")
+
+
+#cast the data types
+consumption$Date = as.Date(consumption$Date, "%d/%m/%Y")
+consumption$Time = as.Date(consumption$Time, "%H:%M:%S")
+
+
+#constraints for the dates
+startDate = as.Date("2007-02-01", "%Y-%m-%d")
+endDate = as.Date("2007-02-02", "%Y-%m-%d")
+ 
+#select subset of data between the startDate and endDate 
+needed_consumption <- subset(consumption, (Date >= startDate & Date <= endDate))
+
+#plot
+
+png(file = "plot4.png", width=480, height=480)
+
+
+par(mfrow=c(2,2))
+
+with(needed_consumption, plot(full_date, Global_active_power, type = "l", 
+                              ylab="Global Active Power (Kilowatt)", xlab = ""))
+
+with(needed_consumption, plot(full_date, Voltage, type = "l", 
+                              ylab="Voltage", xlab = "datetime"))
+
+
+plot(needed_consumption$full_date, needed_consumption$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "")
+lines(needed_consumption$full_date, needed_consumption$Sub_metering_2, type = "l", col = "red")
+lines(needed_consumption$full_date, needed_consumption$Sub_metering_3, type = "l", col = "blue")
+
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1)
+
+with(needed_consumption, plot(full_date, Global_reactive_power, type = "l", 
+                              xlab = "datetime"))
+
+dev.off()
